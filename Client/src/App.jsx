358238dad8c349820f03a8cad545e71f5452adb8
Vicodiru <marve.vicodiru@gmail.com>
2024-05-08 14:53:47 +0100

    Navbar Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,15 +4,35 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 
-import { About, Dashboard, Home, Projects, SignIn, SignUp } from "./pages";
+import {
+  About,
+  Dashboard,
+  Home,
+  HomeLayout,
+  Projects,
+  SignIn,
+  SignUp,
+} from "./pages";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/dashboard", element: <Dashboard /> },
-  { path: "/Sign-in", element: <SignIn /> },
-  { path: "/Sign-up", element: <SignUp /> },
-  { path: "/Projects", element: <Projects /> },
+  {
+    path: "/",
+    element: (
+      <>
+        <HomeLayout />
+        <ScrollRestoration />
+      </>
+    ),
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/Sign-in", element: <SignIn /> },
+      { path: "/Sign-up", element: <SignUp /> },
+      { path: "/Projects", element: <Projects /> },
+    ],
+  },
+  ,
 ]);
 
 function App() {
