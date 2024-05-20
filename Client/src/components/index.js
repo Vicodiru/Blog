836bba6e5ea428ba095051a799e206d17c54f6bd
Vicodiru <marve.vicodiru@gmail.com>
2024-05-20 14:01:15 +0100

    Sidebar Added

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,3 +1,5 @@
-export {default as Header} from "./Header"
-export {default as FooterCom} from "./FooterCom"
-export {default as PrivateRoute} from "./PrivateRoute"
+export { default as Header } from "./Header";
+export { default as FooterCom } from "./FooterCom";
+export { default as PrivateRoute } from "./PrivateRoute";
+export { default as DashSidebar } from "./DashSidebar";
+export { default as DashProfile } from "./DashProfile";
--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { DashSidebar, DashProfile } from "../components/";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
     <div className="min-h-[calc(100vh-320.8px)] grid grid-cols-[1fr_auto]">
-      Dashboard
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-56">
+          <DashSidebar />
+        </div>
+        <div>{tab === "profile" && <DashProfile />}</div>
+      </div>
     </div>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
