3e1c084dea410373c1a5c56f0552967033fb4fb7
Vicodiru <marve.vicodiru@gmail.com>
2024-05-25 14:11:49 +0100

    Show user post inside dashboard

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const DashPosts = () => {
-  return (
-    <div className=''>DashPosts</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {}
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
 
-export default DashPosts
+  console.log(userPosts);
+
+  return (
+    <div className="table-auto  w-auto  md:mx-auto p-3  ">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body className="">
+                <Table.Row className="bg-white divide-y  dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell className="">
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell className="">
+                    <Link to={`/post/${post.slug}`} className="">
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell className="">
+                    <Link
+                      className="font-medium text-gray-900 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell className="">{post.category}</Table.Cell>
+                  <Table.Cell className="">
+                    <span className="font-medium text-red-500 hover:underline cursor pointer">
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell className="">
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit </span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default DashPosts;
--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="min-h-[calc(100vh-320.8px)] grid grid-cols-[1fr_auto]">
+    <div className="min-h-[calc(100vh-320.8px)]  grid grid-cols-[1fr_auto]">
       About
     </div>
   );
